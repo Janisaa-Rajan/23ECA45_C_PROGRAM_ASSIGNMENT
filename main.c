89c5c140d93589e89b02049fda9cc8caaf761c64
Janisaa-Rajan <janisaa.rajan@gmail.com>
2025-03-27 09:46:26 +0530

    testing

---
+++ main.c
@@ -0,0 +1,58 @@
+/*
+ * 🚨 WARNING: CHALLENGE MODE ACTIVATED 🚨
+ * 
+ * 1️⃣ Sit back, take a deep breath, and **THINK**.
+ * 2️⃣ For the next **10 minutes**, avoid using **Gen AI, ChatGPT, or any shortcuts**.
+ * 3️⃣ Trust your **brain**—you’re smarter than you think!
+ * 4️⃣ Consider yourself a **LOSER** if you cheat. Seriously, promise yourself you won't. 😤
+ * 5️⃣ If you solve it on your own, **you WIN** and level up as a true programmer and come and meet me ! 
+ * 
+ * 
+ * 💡 Remember: The best coders aren’t the ones who copy-paste. They are the ones who **struggle, think, fail, and then succeed.** 
+ *    
+ * Now, write your code! You got this! 💪🔥
+ */
+
+#include <stdio.h>
+
+#define MAX_ROOMS 5  //  max number of rooms
+
+// Function prototypes
+void initializeSystem();
+void displayMenu();
+void controlLights();
+void readTemperature();
+void detectMotion();
+void securitySystem();
+void analyzeHouseStatus();
+
+int main() {
+    printf("hello world");  
+    printf("Testing")  ;
+
+    return 0;
+}
+
+ 
+void initializeSystem() {
+  
+}
+
+void displayMenu() {
+   
+}
+void controlLights() {
+
+}
+void readTemperature() {
+
+}
+void detectMotion() {
+
+}
+void securitySystem() {
+
+}
+void analyzeHouseStatus() {
+
+}
